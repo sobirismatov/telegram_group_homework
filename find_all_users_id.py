174756c913163467e0fec6cac5c0f21e7a76e862
sobirismatov <name@uzcoder96@gmail.com>
2022-12-02 16:27:01 +0500

    massege ichidagi userlarni olish

--- find_all_users_id.py
+++ find_all_users_id.py
@@ -1,5 +1,5 @@
 from read_data import read_data
-
+data=read_data("data/result.json")
 def find_all_users_id(data: dict)->list:
     """ 
     This function will find all the users in the json file and return the list of users id
@@ -9,4 +9,12 @@ def find_all_users_id(data: dict)->list:
     Returns:
         list: List containing all the users id
     """
-    return
+    dic=data["messages"]
+    a=[]
+    for i in dic:
+        if i["type"]=="service" and  i['actor_id'] not in a:
+            a.append(i["actor_id"])
+        elif i["type"]=="message" and  i["from_id"] not in a:
+            a.append(i["from_id"])
+    return a
+print(find_all_users_id(data))
