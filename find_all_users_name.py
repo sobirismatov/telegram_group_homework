5937cd07262d5f551007666a8aeb6f49cdca9adc
sobirismatov <name@uzcoder96@gmail.com>
2022-12-02 17:10:15 +0500

    messsagesdagi foydalanuvci nomlari

--- find_all_users_name.py
+++ find_all_users_name.py
@@ -1,5 +1,5 @@
 from read_data import read_data
-
+data=read_data("data/result.json")
 def find_all_users_name(data: dict)->list:
     """
     This function will find all the users in the json file and return the list of users name.
@@ -9,4 +9,12 @@ def find_all_users_name(data: dict)->list:
     Returns:
         list: List containing all the users name.
     """
-    return
+    data=data["messages"]
+    a=[]
+    for i in data:
+        if i["type"]=="service" and i["actor_id"] not in a:
+            a.append(i["actor"])
+        elif i["type"]=="message" and i["from_id"] not in a:
+            a.append(i["from"])
+    return a
+print(find_all_users_name(data))
