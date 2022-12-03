09030e5220c1f7187747b95a50103a2e8a7a3932
sobirismatov <name@uzcoder96@gmail.com>
2022-12-03 21:08:40 +0500

    xabarlar ruyxatidagi id (raqamlar) ni royxatini chiqarish

--- find_number_of_messages.py
+++ find_number_of_messages.py
@@ -1,4 +1,5 @@
 from read_data import read_data
+data=read_data("data/result.json")
 def find_number_of_messages(data: dict)->int:
     """
     Get the total number of messages.
@@ -9,4 +10,14 @@ def find_number_of_messages(data: dict)->int:
         int: Total number of messages.
     
     """
-    return
+    dic=data["messages"]
+    a=[]
+    for i in dic:
+        if i["type"]=="service":
+            if  i["actor_id"] not in a:
+                a.append(i["id"])
+        elif i["type"]=="message":
+            if i["from_id"] not in a:
+                a.append(i["id"])
+    return a
+print(find_number_of_messages(data))
